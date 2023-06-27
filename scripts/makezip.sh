3992d3f24be36be32aa56cbe64a050c291f8683f
Aiono <sahinonur2000@hotmail.com>
2023-06-27 18:54:12 +0300

    chore: Simplify script

--- scripts/makezip.sh
+++ scripts/makezip.sh
@@ -2,9 +2,7 @@
 
 NAME=fullscreen-to-empty-workspace@aiono.dev
 DIR=src
-cd $DIR
-zip -r $NAME.zip *
-mkdir -p ../build
-mv $NAME.zip ../build/$NAME.zip
-cd ..
+zip -r $NAME.zip $DIR/*
+mkdir -p build
+mv $NAME.zip build/$NAME.zip
 
